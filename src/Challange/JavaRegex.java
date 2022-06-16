d9a75292f20b8323fcd8c6abc4d7464e0baaa324
Busra <busraanac4@gmail.com>
2022-06-16 16:40:23 +0300

    HackerRankSolutions

---
+++ src/Challange/JavaRegex.java
@@ -0,0 +1,19 @@
+package Challange;
+
+import java.util.Scanner;
+
+public class JavaRegex {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex{
+    String pattern="\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}";
+}
+
